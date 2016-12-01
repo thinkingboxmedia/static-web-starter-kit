bd0259fb90445fa9b94e25d9079b37b93c8a3b7b
Jean-Michel Saulnier <jmsaulnier@Jean-Michels-MacBook-Pro.local>
2016-12-01 11:09:30 -0800

    Updated package dependencies

--- tools/webpack.base.conf.js
+++ tools/webpack.base.conf.js
@@ -38,7 +38,7 @@ module.exports = {
     loaders: [
       {
         test: /\.js$/,
-        loader: 'babel',
+        loader: 'babel-loader',
         include: projectRoot,
         exclude: /node_modules/,
       },
